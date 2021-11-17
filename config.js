9ca8327516d2a363b9fe41defbea8280a81f2e25
ElainaBot <92618649+Bdjjdjsj@users.noreply.github.com>
2021-11-18 06:43:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
-  '62857139649630': { // put your number here
-    name: 'Owner Metro Bot/Creator games-wabot',
+  '6287836855481': { // put your number here
+    name: 'Owner Elaina-chan/Creator Wong deso',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: false, // if true this number will send if user use command /report or /join
     isCreator: true // if true this number will send if user use command /owner or /creator
@@ -46,15 +46,15 @@ global.APIs = { // API Prefix
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
   'https://api.xteam.xyz': 'd90a9e986e18778b',
-  'https://zahirr-web.herokuapp.com': 'zahirgans',
-  'https://api.zeks.xyz': 'apivinz',
+  'https://zahirr-web.herokuapp.com': 'Ikyy69',
+  'https://api.zeks.xyz': 'NYXNGONTOL',
   'https://pencarikode.xyz': 'pais',
   'http://hardianto-chan.herokuapp.com': 'hardianto'
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot Is The Best'
+global.packname = 'wa.me/6287836855481'
+global.author = 'wong deso'
 
 global.multiplier = 39 // The higher, The harder levelup
 
